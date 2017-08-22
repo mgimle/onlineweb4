4286edf53de38943ed6305cfeb0f429d78ca41a6
MrHanSan <hanssandbu@gmail.com>
2017-08-22 22:06:01 +0200

    added mapping for unique field names for text and rating questions

--- apps/feedback/dashboard/forms.py
+++ apps/feedback/dashboard/forms.py
@@ -3,6 +3,17 @@ from django import forms
 
 from apps.feedback.models import Feedback, TextQuestion, RatingQuestion, MultipleChoiceQuestion
 
+TEXT_QUESTION_NAME_MAP = {
+    'order': 'text_order',
+    'label': 'text_label',
+    'display': 'text_display',
+}
+
+RATING_QUESTION_NAME_MAP = {
+    'order': 'rating_order',
+    'label': 'rating_label',
+    'display': 'rating_display'
+}
 
 
 class FeedbackForm(forms.ModelForm):
@@ -14,7 +25,15 @@ class FeedbackForm(forms.ModelForm):
             'display_info',
         ]
 
+
 class FeedbackQuestionForm(forms.ModelForm):
+    def add_prefix(self, field_name):
+            # Looks up html name for field in *_NAME_MAP
+            # The HTML name is needed to differentiate between
+            # rating and text field names
+            field_name = TEXT_QUESTION_NAME_MAP.get(field_name, field_name)
+            return super(FeedbackQuestionForm, self).add_prefix(field_name)
+
     class Meta(object):
         model = TextQuestion
         fields = (
@@ -23,7 +42,12 @@ class FeedbackQuestionForm(forms.ModelForm):
             'display',
         )
 
+
 class FeedbackRatingForm(forms.ModelForm):
+    def add_prefix(self, field_name):
+        field_name = RATING_QUESTION_NAME_MAP.get(field_name, field_name)
+        return super(FeedbackRatingForm, self).add_prefix(field_name)
+
     class Meta(object):
         model = RatingQuestion
         fields = (
