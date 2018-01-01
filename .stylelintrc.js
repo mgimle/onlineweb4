e01610a5bad9fbd056f34391a2830724f6fd5e85
sardred <andelar96@gmail.com>
2018-01-01 01:18:19 +0100

    Removed stylelint-selector-pem-pattern, it was way too primitive

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,19 +1,6 @@
 module.exports = {
   extends: "stylelint-config-standard",
-  plugins: [
-    "stylelint-selector-bem-pattern"
-  ],
   rules: {
     "selector-list-comma-newline-after": "always-multi-line",
-    "plugin/selector-bem-pattern": {
-      preset: "bem",
-      componentSelectors: function(componentName) {
-        var WORD = '[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*';
-        var element = '(?:__' + WORD + ')?';
-        var modifier = '(?:--' + WORD + '){0,2}';
-        var attribute = '(?:\\[.+\\])?';
-        return new RegExp('^\\.' + componentName + element + modifier + attribute + '$');
-      }
-    }
   }
 }
